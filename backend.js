e3f50116274db36278dc715ab30e195303a7823d
Timberland9x <choistewart9x@yahoo.com>
2023-10-16 02:03:47 -0700

    js code for current page highlight

--- backend.js
+++ backend.js
@@ -2,3 +2,9 @@ $("a").click(function(){
     $("a").removeClass("active");
         $(this).addClass("active");
    });
+
+function project()
+{
+    document.getElementsByClassName("navUl").style.color = "red";
+    console.log("project test")
+};
