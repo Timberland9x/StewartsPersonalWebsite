f23ccdcfed20a33691eff851c66c87e64d655440
Timberland9x <choistewart9x@yahoo.com>
2023-11-07 15:46:51 -0800

    added to top button

--- backend.js
+++ backend.js
@@ -8,3 +8,24 @@ forEach(link => {
     } 
 }
 )
+
+
+// Get the button:
+let topBtn = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+}
